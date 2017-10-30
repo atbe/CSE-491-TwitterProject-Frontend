fb5f2694f27e4dd4b09053f9052ece6ec37cd785
@be <abeahmed2@gmail.com>
2017-10-29 21:00:18 -0400

    Moved project to angular.

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyCeUodoNfxzpThVhg4IRdg0lXfOwuGuMB8',
+  authDomain: 'twittertweettracker.firebaseapp.com',
+  databaseURL: 'https://twittertweettracker.firebaseio.com',
+  projectId: 'twittertweettracker',
+  storageBucket: 'twittertweettracker.appspot.com',
+  messagingSenderId: '785137017428'
+};
