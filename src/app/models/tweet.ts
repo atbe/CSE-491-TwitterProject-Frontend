768519e29b2e7f77823ee4d368dd5cac252f6c77
@be <abeahmed2@gmail.com>
2017-11-21 21:37:10 -0500

    Updated models.

--- src/app/models/tweet.ts
+++ src/app/models/tweet.ts
@@ -1,38 +1,65 @@
 import { User } from './user';
 
+export interface Hashtag {
+  text: string;
+  indices: number[];
+}
+
+export interface Url {
+  url: string;
+  expanded_url: string;
+  display_url: string;
+  indices: number[];
+}
+
 export interface Entities {
-  hashtags: any[];
+  hashtags: Hashtag[];
   symbols: any[];
-  urls: any[];
   user_mentions: any[];
+  urls: Url[];
+}
+
+export interface BoundingBox {
+  type: string;
+  coordinates: number[][][];
+}
+
+export interface Place {
+  id: string;
+  url: string;
+  place_type: string;
+  name: string;
+  full_name: string;
+  country_code: string;
+  country: string;
+  contained_within: any[];
+  bounding_box: BoundingBox;
+  attributes: any;
 }
 
 export interface Tweet {
-    contributors?: any;
-    coordinates?: any;
-    created_at: string;
-    entities: Entities;
-    favorite_count: number;
-    favorited: boolean;
-    filter_level: string;
-    geo?: any;
-    id: number;
-    id_str: string;
-    in_reply_to_screen_name?: any;
-    in_reply_to_status_id?: any;
-    in_reply_to_status_id_str?: any;
-    in_reply_to_user_id?: any;
-    in_reply_to_user_id_str?: any;
-    is_quote_status: boolean;
-    lang: string;
-    place?: any;
-    quote_count: number;
-    reply_count: number;
-    retweet_count: number;
-    retweeted: boolean;
-    source: string;
-    text: string;
-    timestamp_ms: string;
-    truncated: boolean;
-    user: User;
+  created_at?: string;
+  id: number;
+  id_str: string;
+  text?: string;
+  truncated?: boolean;
+  entities?: Entities;
+  source?: string;
+  in_reply_to_status_id?: any;
+  in_reply_to_status_id_str?: any;
+  in_reply_to_user_id?: any;
+  in_reply_to_user_id_str?: any;
+  in_reply_to_screen_name?: any;
+  user?: User;
+  geo?: any;
+  coordinates?: any;
+  place?: Place;
+  contributors?: any;
+  is_quote_status?: boolean;
+  retweet_count?: number;
+  favorite_count?: number;
+  favorited?: boolean;
+  retweeted?: boolean;
+  lang?: string;
 }
+
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,40 +1,55 @@
+import { Url } from './tweet';
+
+export interface Description {
+  urls: Url[];
+}
+
+export interface UserEntities {
+  urls: Url[];
+  description: Description;
+}
+
 export interface User {
-  contributors_enabled: boolean;
-  created_at: string;
-  default_profile: boolean;
-  default_profile_image: boolean;
-  description?: any;
-  favourites_count: number;
-  follow_request_sent?: any;
-  followers_count: number;
-  following?: any;
-  friends_count: number;
-  geo_enabled: boolean;
   id: number;
   id_str: string;
-  is_translator: boolean;
-  lang: string;
-  listed_count: number;
-  location: string;
   name: string;
-  notifications?: any;
+  screen_name: string;
+  location: string;
+  description: string;
+  url: string;
+  entities: UserEntities;
+  protected: boolean;
+  followers_count: number;
+  friends_count: number;
+  listed_count: number;
+  created_at: string;
+  favourites_count: number;
+  utc_offset: number;
+  time_zone: string;
+  geo_enabled: boolean;
+  verified: boolean;
+  statuses_count: number;
+  lang: string;
+  contributors_enabled: boolean;
+  is_translator: boolean;
+  is_translation_enabled: boolean;
   profile_background_color: string;
   profile_background_image_url: string;
   profile_background_image_url_https: string;
   profile_background_tile: boolean;
   profile_image_url: string;
   profile_image_url_https: string;
+  profile_banner_url: string;
   profile_link_color: string;
   profile_sidebar_border_color: string;
   profile_sidebar_fill_color: string;
   profile_text_color: string;
   profile_use_background_image: boolean;
-  protected: boolean;
-  screen_name: string;
-  statuses_count: number;
-  time_zone?: any;
+  has_extended_profile: boolean;
+  default_profile: boolean;
+  default_profile_image: boolean;
+  following: boolean;
+  follow_request_sent: boolean;
+  notifications: boolean;
   translator_type: string;
-  url?: any;
-  utc_offset?: any;
-  verified: boolean;
 }
